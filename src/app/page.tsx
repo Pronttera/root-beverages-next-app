7b64429986895dd8652b9b8fcdc011234e86cb83
Arbaj Ansari <arbaj897ansari@gmail.com>
2026-04-07 21:24:59 +0530

    hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import AppBar from "./components/layout/AppBar";
 import Footer from "./components/layout/Footer";
 
+import HeroSection from "./sections/HeroSection";
 import AboutSection from "./sections/AboutSection";
+import TestimonialSection from "./sections/TestimonialSection";
 
 // TODO: Add Meta Here
 export default function Home() {
@@ -9,7 +11,9 @@ export default function Home() {
     <>
       <AppBar />
 
+      <HeroSection />
       <AboutSection />
+      <TestimonialSection />
       {/* <Footer /> */}
     </>
   );
